86d3029ca05516febbe19e3131ae14e4d76b3226
nausher123 <52681182+nausher123@users.noreply.github.com>
2022-11-18 00:02:07 +0500

    Delete testdjango directory

--- testdjango/markcheck/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MarkcheckConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'markcheck'
--- testdjango/markcheck/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path("", views.home, name= "home"),
-    path("alevel/", views.alevel, name= "alevel"),
-    path("olevel/", views.olevel, name="olevel"),
-    path("sat/", views.sat, name="sat"),
-]
